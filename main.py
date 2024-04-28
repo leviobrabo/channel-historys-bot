5992891a7d670546f1524ec6a91b7c0306b7cb2b
leviobrabo <carlosjunior20313@gmail.com>
2024-04-28 20:33:18 -0300

    dsada

--- main.py
+++ main.py
@@ -36,13 +36,13 @@ schedule.every().day.at('11:00').do(hist_CHANNEL_AR_imgs)
 schedule.every().day.at('15:00').do(hist_CHANNEL_AR_death)
 schedule.every().day.at('22:00').do(hist_CHANNEL_AR_birth)
 schedule.every().friday.at('00:00').do(message_CHANNEL_AR_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_AR)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_AR)
 schedule.every().day.at('20:40').do(send_president_photo_AR)
 
 # czech
 schedule.every().day.at('05:00').do(hist_CHANNEL_CS_events)
 schedule.every().friday.at('00:00').do(message_CHANNEL_CS_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_CS)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_CS)
 schedule.every().day.at('20:40').do(send_president_photo_CS)
 
 # german
@@ -51,7 +51,7 @@ schedule.every().day.at('11:00').do(hist_CHANNEL_DE_imgs)
 schedule.every().day.at('15:00').do(hist_CHANNEL_DE_death)
 schedule.every().day.at('22:00').do(hist_CHANNEL_DE_birth)
 schedule.every().friday.at('00:00').do(message_CHANNEL_DE_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_DE)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_DE)
 schedule.every().day.at('20:40').do(send_president_photo_DE)
 
 
@@ -61,7 +61,7 @@ schedule.every().day.at('11:00').do(hist_CHANNEL_ES_imgs)
 schedule.every().day.at('15:00').do(hist_CHANNEL_ES_death)
 schedule.every().day.at('22:00').do(hist_CHANNEL_ES_birth)
 schedule.every().friday.at('00:00').do(message_CHANNEL_ES_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_ES)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_ES)
 schedule.every().day.at('20:40').do(send_president_photo_ES)
 
 
@@ -71,32 +71,32 @@ schedule.every().day.at('11:00').do(hist_CHANNEL_FR_imgs)
 schedule.every().day.at('15:00').do(hist_CHANNEL_FR_death)
 schedule.every().day.at('22:00').do(hist_CHANNEL_FR_birth)
 schedule.every().friday.at('00:00').do(message_CHANNEL_FR_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_FR)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_FR)
 schedule.every().day.at('20:40').do(send_president_photo_FR)
 
 # hindi
 schedule.every().day.at('05:00').do(hist_CHANNEL_HI_events)
 schedule.every().friday.at('00:00').do(message_CHANNEL_HI_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_HI)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_HI)
 schedule.every().day.at('20:40').do(send_president_photo_HI)
 
 # indonesian
 schedule.every().day.at('05:00').do(hist_CHANNEL_ID_events)
 schedule.every().friday.at('00:00').do(message_CHANNEL_ID_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_ID)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_ID)
 schedule.every().day.at('20:40').do(send_president_photo_ID)
 
 # italian
 schedule.every().day.at('05:00').do(hist_CHANNEL_IT_events)
 schedule.every().day.at('22:00').do(hist_CHANNEL_IT_imgs)
 schedule.every().friday.at('00:00').do(message_CHANNEL_IT_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_IT)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_IT)
 schedule.every().day.at('20:40').do(send_president_photo_IT)
 
 # japanese
 schedule.every().day.at('05:00').do(hist_CHANNEL_JA_events)
 schedule.every().friday.at('00:00').do(message_CHANNEL_JA_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_JA)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_JA)
 schedule.every().day.at('20:40').do(send_president_photo_JA)
 
 # korean
@@ -106,7 +106,7 @@ schedule.every().friday.at('00:00').do(message_CHANNEL_KO_alert)
 # polish
 schedule.every().day.at('05:00').do(hist_CHANNEL_PL_events)
 schedule.every().friday.at('00:00').do(message_CHANNEL_PL_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_PL)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_PL)
 schedule.every().day.at('20:40').do(send_president_photo_PL)
 
 # russian
@@ -115,13 +115,13 @@ schedule.every().day.at('11:00').do(hist_CHANNEL_RU_imgs)
 schedule.every().day.at('15:00').do(hist_CHANNEL_RU_death)
 schedule.every().day.at('22:00').do(hist_CHANNEL_RU_birth)
 schedule.every().friday.at('00:00').do(message_CHANNEL_RU_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_RU)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_RU)
 schedule.every().day.at('20:40').do(send_president_photo_RU)
 
 # turkish
 schedule.every().day.at('05:00').do(hist_CHANNEL_TR_events)
 schedule.every().friday.at('00:00').do(message_CHANNEL_TR_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_TR)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_TR)
 schedule.every().day.at('20:40').do(send_president_photo_TR)
 
 
@@ -131,13 +131,13 @@ schedule.every().day.at('11:00').do(hist_CHANNEL_UK_imgs)
 schedule.every().day.at('15:00').do(hist_CHANNEL_UK_death)
 schedule.every().day.at('22:00').do(hist_CHANNEL_UK_birth)
 schedule.every().friday.at('00:00').do(message_CHANNEL_UK_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_UK)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_UK)
 schedule.every().day.at('20:40').do(send_president_photo_UK)
 
 # vietnamese
 schedule.every().day.at('05:00').do(hist_CHANNEL_VI_events)
 schedule.every().friday.at('00:00').do(message_CHANNEL_VI_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_VI)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_VI)
 schedule.every().day.at('20:40').do(send_president_photo_VI)
 
 # chinese
@@ -146,7 +146,7 @@ schedule.every().day.at('11:00').do(hist_CHANNEL_ZH_imgs)
 schedule.every().day.at('15:00').do(hist_CHANNEL_ZH_death)
 schedule.every().day.at('22:00').do(hist_CHANNEL_ZH_birth)
 schedule.every().friday.at('00:00').do(message_CHANNEL_ZH_alert)
-schedule.every().day.at('08:00').do(hist_channel_curiosity_ZH)
+schedule.every().day.at('21:30').do(hist_channel_curiosity_ZH)
 schedule.every().day.at('20:40').do(send_president_photo_ZH)
 
 
